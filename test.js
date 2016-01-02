79d5d1f6cad2b4ca094e8b02f7cefb2f946e7b48
andrepolischuk <andre.polischuk@gmail.com>
2016-01-02 22:16:09 +0300

    Bump deps

--- test.js
+++ test.js
@@ -1,26 +1,26 @@
 import test from 'ava';
-import approximatelyEqual from 'approximately-equal';
-import coordinates from './index';
+import equal from 'approximately-equal';
+import coordinates from './index.es5';
 
-test('shoild return default origin', async t => {
+test('return default origin', t => {
   const {x, y, r} = coordinates({x: 0, y: 0});
   t.same({x, y, r}, {x: 0, y: 0, r: 0});
 });
 
-test('shoild return specified origin', async t => {
+test('return specified origin', t => {
   const {x, y, r} = coordinates({x: 100, y: 100}, {x: 100, y: 100});
   t.same({x, y, r}, {x: 0, y: 0, r: 0});
 });
 
-test('shoild return coordinates relative to default origin', async t => {
+test('return coordinates relative to default origin', t => {
   const {x, y, r, fi} = coordinates({x: 100, y: 100});
   t.same({x, y}, {x: 100, y: 100});
-  t.true(approximatelyEqual(r, 141.42, 0.01));
-  t.true(approximatelyEqual(fi, Math.PI / 4, 0.01));
+  t.true(equal(r, 141.42, 0.01));
+  t.true(equal(fi, Math.PI / 4, 0.01));
 });
 
-test('shoild return coordinates relative to specified origin', async t => {
+test('return coordinates relative to specified origin', t => {
   const {x, y, r, fi} = coordinates({x: 50, y: 100}, {x: 100, y: 100});
   t.same({x, y, r}, {x: -50, y: 0, r: 50});
-  t.true(approximatelyEqual(fi, Math.PI, 0.01));
+  t.true(equal(fi, Math.PI, 0.01));
 });
